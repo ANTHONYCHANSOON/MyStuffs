0ebc77738eb7b0db548ab870ac61304148913473
anthony soon <anthonychansoon@gmail.com>
2019-12-21 14:43:11 -0800

    server files and html routes

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const connection = mysql.createConnection ({
+    host : "localhost",
+    user: "root",
+    password: "!Anthon69",
+    database : "mystuffdb"
+})
+
+connection.connect(function(err) {
+    if(err) {
+        console.log(err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId)
+})
+
 // require("./routes/mainstuff.js")(app);
 // require("./routes/substuff.js")(app);
 // require("./routes/user-api")(app);
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log(`App running on port ${PORT}`)
